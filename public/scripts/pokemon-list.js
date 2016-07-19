8da8130c609777a424228e8e0a4eb2323d5b047d
justinleewells <justin.wells@nimbusnine.co>
2016-07-18 18:44:40 -0600

    Added initial front-end files

---
+++ public/scripts/pokemon-list.js
@@ -0,0 +1,12 @@
+var PokemonList = React.createClass({
+  render: function() {
+    return (
+      <div className="pokemonList">
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <PokemonList />,
+  document.getElementById('content')
+);
