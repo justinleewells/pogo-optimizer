fb681498861841970b449cc1112248322d989bc0
justinleewells <justinleewells@icloud.com>
2016-07-22 18:44:13 -0600

    First pass at refactorization

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
  * Express
  */
 
-var jsf = require('jsonfile');
+var _       = require('lodash');
+var fs      = require('fs');
+var jsf     = require('jsonfile');
 var express = require('express');
-var app = express();
-var fs = require('fs');
+var app     = express();
 
 jsf.writeFileSync('./data/inventory.json', [], {}, function (err) {
   if (err) console.log(err);
@@ -34,82 +35,118 @@ app.listen(3000, function () {
  * Mitm
  */
 
-var _ = require('lodash');
 var PokemonGoMITM = require('pokemon-go-mitm');
-var PokemonData = require('./data/pokemon.json');
-var releasing_id = null;
 
-var server = new PokemonGoMITM({
-  port: 8081
-})
-.setResponseHandler("GetInventory", function(data) {
-  var formatted, tmp;
-  tmp = data.inventory_delta.inventory_items;
-  if (tmp.length > 0) {
-    formatted = _.reduce(tmp, function(result, entry) {
-      if (entry.inventory_item_data && entry.inventory_item_data.pokemon_data && !entry.inventory_item_data.pokemon_data.is_egg) {
-        result.push(entry.inventory_item_data.pokemon_data);
-      }
-      return result;
-    }, []);
-    formatted = _.map(formatted, function (entry) {
-      if (entry.individual_stamina === undefined) entry.individual_stamina = 0;
-      if (entry.individual_attack === undefined)  entry.individual_attack  = 0;
-      if (entry.individual_defense === undefined) entry.individual_defense = 0;
-      entry.power_quotient = (entry.individual_stamina + entry.individual_attack + entry.individual_defense) / 45;
-      var data = _.find(PokemonData, function (pokemon) {
-        return (pokemon.Name.toUpperCase() == entry.pokemon_id || pokemon.AltName == entry.pokemon_id);
-      });
-      if (data != null) {
-        entry.pokedex_id = parseInt(data.Number);
-	      if (entry.nickname === undefined) entry.nickname = data["Name"];
-        entry.type_1 = data["Type I"];
-        if (data["Type II"]) entry.type_2 = data["Type II"];
-      }
-      entry.move_1 = formatMoveName(entry.move_1);
-      entry.move_2 = formatMoveName(entry.move_2);
-      return entry;
-    });
-    if (formatted.length > 0) {
-      var inventory = jsf.readFileSync('./data/inventory.json');
-      if (inventory.length > formatted.length) formatted = inventory.concat(formatted);
-      jsf.writeFile("./data/inventory.json", formatted, {spaces: 2}, function(err) {
-        if (err != null) console.log(err);
-      });
+// var server = new PokemonGoMITM({
+//   port: 8081
+// })
+// .setResponseHandler("GetInventory", function(data) {
+//   var formatted, tmp;
+//   tmp = data.inventory_delta.inventory_items;
+//   if (tmp.length > 0) {
+//     formatted = _.reduce(tmp, function(result, entry) {
+//       if (entry.inventory_item_data && entry.inventory_item_data.pokemon_data && !entry.inventory_item_data.pokemon_data.is_egg) {
+//         result.push(entry.inventory_item_data.pokemon_data);
+//       }
+//       return result;
+//     }, []);
+//     formatted = _.map(formatted, function (entry) {
+//       if (entry.individual_stamina === undefined) entry.individual_stamina = 0;
+//       if (entry.individual_attack === undefined)  entry.individual_attack  = 0;
+//       if (entry.individual_defense === undefined) entry.individual_defense = 0;
+//       entry.power_quotient = (entry.individual_stamina + entry.individual_attack + entry.individual_defense) / 45;
+//       var data = _.find(PokemonData, function (pokemon) {
+//         return (pokemon.Name.toUpperCase() == entry.pokemon_id || pokemon.AltName == entry.pokemon_id);
+//       });
+//       if (data != null) {
+//         entry.pokedex_id = parseInt(data.Number);
+// 	      if (entry.nickname === undefined) entry.nickname = data["Name"];
+//         entry.type_1 = data["Type I"];
+//         if (data["Type II"]) entry.type_2 = data["Type II"];
+//       }
+//       entry.move_1 = formatMoveName(entry.move_1);
+//       entry.move_2 = formatMoveName(entry.move_2);
+//       return entry;
+//     });
+//     if (formatted.length > 0) {
+//       var inventory = jsf.readFileSync('./data/inventory.json');
+//       if (inventory.length > formatted.length) formatted = inventory.concat(formatted);
+//       jsf.writeFile("./data/inventory.json", formatted, {spaces: 2}, function(err) {
+//         if (err != null) console.log(err);
+//       });
+//     }
+//   }
+//   return data;
+// })
+// .setRequestHandler("ReleasePokemon", function (data) {
+//   releasing_id = data.pokemon_id;
+// })
+// .setResponseHandler("ReleasePokemon", function(data) {
+//   if (data.result === 'SUCCESS' && releasing_id !== null) {
+//     var inventory = jsf.readFileSync('./data/inventory.json');
+//     _.remove(inventory, {id: releasing_id});
+//     releasing_id = null;
+//     jsf.writeFile("./data/inventory.json", inventory, {spaces: 2}, function(err) {
+//       if (err != null) console.log(err);
+//     });
+//   }
+// });
+//
+// /**
+//  * Utility functions
+//  */
+//
+// function capitalize(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+//
+// function formatMoveName(name) {
+//   var ret = '';
+//   var spl = name.split('_');
+//   _.forEach(spl, function(str) {
+//     if (str !== 'FAST') {
+//       if (ret !== '') ret += ' ';
+//       ret += capitalize(str.toLowerCase());
+//     }
+//   });
+//   return ret;
+// }
+
+var Player    = require('./lib/player');
+var Inventory = require('./lib/inventory');
+
+var stage     = 0;
+var player    = null;
+var inventory = null;
+var changes   = null;
+
+new PokemonGoMITM({port: 8081})
+  .setResponseHandler('GetPlayer', function (data) {
+    if (data.success && player === null) {
+      player = new Player(data.player_data.username);
+      player.load(data.player_data);
+      player.save();
+      stage = 1;
     }
-  }
-  return data;
-})
-.setRequestHandler("ReleasePokemon", function (data) {
-  releasing_id = data.pokemon_id;
-})
-.setResponseHandler("ReleasePokemon", function(data) {
-  if (data.result === 'SUCCESS' && releasing_id !== null) {
-    var inventory = jsf.readFileSync('./data/inventory.json');
-    _.remove(inventory, {id: releasing_id});
-    releasing_id = null;
-    jsf.writeFile("./data/inventory.json", inventory, {spaces: 2}, function(err) {
-      if (err != null) console.log(err);
-    });
-  }
-});
-
-/**
- * Utility functions
- */
-
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function formatMoveName(name) {
-  var ret = '';
-  var spl = name.split('_');
-  _.forEach(spl, function(str) {
-    if (str !== 'FAST') {
-      if (ret !== '') ret += ' ';
-      ret += capitalize(str.toLowerCase());
+  })
+  .setResponseHandler('GetInventory', function (data) {
+    if (data.success) {
+      var delta = data.inventory_delta;
+      var pokemon = _.reduce(delta.inventory_items, function (result, item) {
+        return (item.inventory_item_data.pokemon_data !== undefined) ? _.concat(result, item.inventory_item_data.pokemon_data) : result;
+      }, []);
+      var candy = _.reduce(delta.inventory_items, function (result, item) {
+        // this is unnecessarily complicated because I had a bugged pidgey entry
+        // in conclusion: pidgey is an asshole
+        return (item.inventory_item_data.pokemon_family !== undefined &&
+               !item.inventory_item_data.pokemon_family.inventory_item_data) ? _.concat(result, item.inventory_item_data.pokemon_family) : result;
+      });
+      if (inventory === null) {
+        inventory = new Inventory(player.username);
+        inventory.load(candy, pokemon);
+        inventory.save();
+      } else {
+        
+      }
     }
   });
-  return ret;
-}
