5fb409d1a84942713b738d4e6701856a405a5955
justinleewells <justinleewells@icloud.com>
2016-07-23 11:21:12 -0600

    Implemented new inventory system + player data queries

--- index.js
+++ index.js
@@ -1,116 +1,8 @@
 /**
- * Express
- */
-
-var _       = require('lodash');
-var fs      = require('fs');
-var jsf     = require('jsonfile');
-var express = require('express');
-var app     = express();
-
-jsf.writeFileSync('./data/inventory.json', [], {}, function (err) {
-  if (err) console.log(err);
-});
-
-app.use(express.static('public'));
-app.use("/ca.pem", express.static('.http-mitm-proxy/certs/ca.pem'));
-
-app.get('/api/pokemon', function (req, res, next) {
-  jsf.readFile("./data/inventory.json", function (err, data) {
-    if (err) return next(err);
-    res.send(data);
-    next();
-  });
-});
-
-app.listen(3000, function () {
-  console.log('Pokemon GO Optimizer front-end listening on port 3000');
-
-  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-    console.log('Accept the cert at ' + add + ':3000/ca.pem if you haven\'t already done so');
-  });
-});
-
-/**
  * Mitm
  */
 
 var PokemonGoMITM = require('pokemon-go-mitm');
-
-// var server = new PokemonGoMITM({
-//   port: 8081
-// })
-// .setResponseHandler("GetInventory", function(data) {
-//   var formatted, tmp;
-//   tmp = data.inventory_delta.inventory_items;
-//   if (tmp.length > 0) {
-//     formatted = _.reduce(tmp, function(result, entry) {
-//       if (entry.inventory_item_data && entry.inventory_item_data.pokemon_data && !entry.inventory_item_data.pokemon_data.is_egg) {
-//         result.push(entry.inventory_item_data.pokemon_data);
-//       }
-//       return result;
-//     }, []);
-//     formatted = _.map(formatted, function (entry) {
-//       if (entry.individual_stamina === undefined) entry.individual_stamina = 0;
-//       if (entry.individual_attack === undefined)  entry.individual_attack  = 0;
-//       if (entry.individual_defense === undefined) entry.individual_defense = 0;
-//       entry.power_quotient = (entry.individual_stamina + entry.individual_attack + entry.individual_defense) / 45;
-//       var data = _.find(PokemonData, function (pokemon) {
-//         return (pokemon.Name.toUpperCase() == entry.pokemon_id || pokemon.AltName == entry.pokemon_id);
-//       });
-//       if (data != null) {
-//         entry.pokedex_id = parseInt(data.Number);
-// 	      if (entry.nickname === undefined) entry.nickname = data["Name"];
-//         entry.type_1 = data["Type I"];
-//         if (data["Type II"]) entry.type_2 = data["Type II"];
-//       }
-//       entry.move_1 = formatMoveName(entry.move_1);
-//       entry.move_2 = formatMoveName(entry.move_2);
-//       return entry;
-//     });
-//     if (formatted.length > 0) {
-//       var inventory = jsf.readFileSync('./data/inventory.json');
-//       if (inventory.length > formatted.length) formatted = inventory.concat(formatted);
-//       jsf.writeFile("./data/inventory.json", formatted, {spaces: 2}, function(err) {
-//         if (err != null) console.log(err);
-//       });
-//     }
-//   }
-//   return data;
-// })
-// .setRequestHandler("ReleasePokemon", function (data) {
-//   releasing_id = data.pokemon_id;
-// })
-// .setResponseHandler("ReleasePokemon", function(data) {
-//   if (data.result === 'SUCCESS' && releasing_id !== null) {
-//     var inventory = jsf.readFileSync('./data/inventory.json');
-//     _.remove(inventory, {id: releasing_id});
-//     releasing_id = null;
-//     jsf.writeFile("./data/inventory.json", inventory, {spaces: 2}, function(err) {
-//       if (err != null) console.log(err);
-//     });
-//   }
-// });
-//
-// /**
-//  * Utility functions
-//  */
-//
-// function capitalize(string) {
-//   return string.charAt(0).toUpperCase() + string.slice(1);
-// }
-//
-// function formatMoveName(name) {
-//   var ret = '';
-//   var spl = name.split('_');
-//   _.forEach(spl, function(str) {
-//     if (str !== 'FAST') {
-//       if (ret !== '') ret += ' ';
-//       ret += capitalize(str.toLowerCase());
-//     }
-//   });
-//   return ret;
-// }
 
 var Player    = require('./lib/player');
 var Inventory = require('./lib/inventory');
@@ -118,7 +10,7 @@ var Inventory = require('./lib/inventory');
 var stage     = 0;
 var player    = null;
 var inventory = null;
-var changes   = null;
+var releasing = null;
 
 new PokemonGoMITM({port: 8081})
   .setResponseHandler('GetPlayer', function (data) {
@@ -136,17 +28,60 @@ new PokemonGoMITM({port: 8081})
         return (item.inventory_item_data.pokemon_data !== undefined) ? _.concat(result, item.inventory_item_data.pokemon_data) : result;
       }, []);
       var candy = _.reduce(delta.inventory_items, function (result, item) {
-        // this is unnecessarily complicated because I had a bugged pidgey entry
-        // in conclusion: pidgey is an asshole
-        return (item.inventory_item_data.pokemon_family !== undefined &&
-               !item.inventory_item_data.pokemon_family.inventory_item_data) ? _.concat(result, item.inventory_item_data.pokemon_family) : result;
-      });
+        return (item.inventory_item_data.pokemon_family !== undefined) ? _.concat(result, item.inventory_item_data.pokemon_family) : result;
+      }, []);
       if (inventory === null) {
         inventory = new Inventory(player.username);
         inventory.load(candy, pokemon);
         inventory.save();
       } else {
-        
+        inventory.update(candy, pokemon);
+        inventory.save();
       }
     }
+  })
+  .setRequestHandler("ReleasePokemon", function (data) {
+    releasing = data.pokemon_id;
+  })
+  .setResponseHandler("ReleasePokemon", function(data) {
+    if (data.result === 'SUCCESS' && releasing !== null) {
+      inventory.release(releasing);
+      inventory.save();
+      releasing = null;
+    }
   });
+
+/**
+ * Express
+ */
+
+var _       = require('lodash');
+var fs      = require('fs');
+var jsf     = require('jsonfile');
+var express = require('express');
+var app     = express();
+
+jsf.writeFileSync('./data/inventory.json', [], {}, function (err) {
+  if (err) console.log(err);
+});
+
+app.use(express.static('public'));
+app.use("/ca.pem", express.static('.http-mitm-proxy/certs/ca.pem'));
+
+app.get('/api/player', function (req, res, next) {
+  if (player) return res.send(player.toJSON());
+  else res.send({});
+});
+
+app.get('/api/inventory', function (req, res, next) {
+  if (inventory) return res.send(inventory.toJSON());
+  else res.send({});
+});
+
+app.listen(3000, function () {
+  console.log('Pokemon GO Optimizer front-end listening on port 3000');
+
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('Accept the cert at ' + add + ':3000/ca.pem if you haven\'t already done so');
+  });
+});
