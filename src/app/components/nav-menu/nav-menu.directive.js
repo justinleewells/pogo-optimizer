2b95b20b1ccf0ab3fc4636a72c78c499a29e04a1
justinleewells <justinleewells@icloud.com>
2016-07-23 19:21:44 -0600

    Implemented nav menu

--- src/app/components/nav-menu/nav-menu.directive.js
+++ src/app/components/nav-menu/nav-menu.directive.js
@@ -5,8 +5,10 @@ angular.module('optimizer.nav-menu.directive', [])
       restrict: 'E',
       scope: true,
       templateUrl: 'app/components/nav-menu/nav-menu.html',
-      controller: ['$scope', function ($scope) {
-        console.log('testd');
+      controller: ['$scope', '$state', function ($scope, $state) {
+        $scope.display = function () {
+          return $state.current.name !== 'connect';
+        }
       }]
     };
 
