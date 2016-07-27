65ba28fdcc20a728eafc22fcd01131f2560161f9
justinleewells <justinleewells@icloud.com>
2016-07-26 18:30:00 -0600

    Code clean up

--- src/app/components/spreadsheet/spreadsheet.controller.js
+++ src/app/components/spreadsheet/spreadsheet.controller.js
@@ -1,52 +1,23 @@
 angular.module('optimizer.spreadsheet.controller', [])
   .controller('SpreadsheetCtrl', ['$scope', '$rootScope', '$timeout', 'APIService',
     function($scope, $rootScope, $timeout, APIService) {
-
+      
       /**
        * Initialization
        */
-
+      
       $timeout(function () {
         $('table').stickyTableHeaders({fixedOffset: $('#nav-menu')});
       }, 10);
-
-      $scope.selected = null;
-      $scope.favoriteClass = function (pokemon) {
-        var ret = '';
-        if (pokemon.data.favorite) ret = 'favorite';
-        return ret;
+      
+      /**
+       * Spreadsheet Settings Modal
+       */
+      
+      $scope.openSettingsModal = function () {
+        $('#spreadsheet-settings-modal').modal('show');
       };
-      $scope.formatPercentage = function (float) {
-        return Math.round(float * 100);
-      };
-      $scope.formatMetric = function (float) {
-        return Math.round(float * 100) / 100;
-      };
-      $scope.formatMoveName = function (name) {
-        var ret = '';
-        var spl = name.split('_');
-        for (var i = 0; i < spl.length; i++) {
-          if (i !== 0 && spl[i] !== 'FAST') ret += ' ';
-          if (spl[i] !== 'FAST') ret += spl[i].toLowerCase();
-        }
-        return ret;
-      };
-      $scope.getMoveDPS = function (move) {
-        var ret = 0;
-        if (move.Power && move.DurationMs) {
-          var dps = parseInt(move.Power)/parseInt(move.DurationMs)*1000;
-          ret = dps.toFixed(2);
-        }
-        return ret;
-      };
-      $scope.openPokemonModal = function (pokemon) {
-        $scope.selected = pokemon;
-        $('#pokemon-modal').modal('show');
-      };
-      $scope.openSettingsModal = function (pokemon) {
-        $scope.selected = pokemon;
-        $('.ui.modal.settings').modal('show');
-      };
+      
       $scope.updateSettings = function () {
         APIService.settings($rootScope.player.settings).then(
           function () {},
@@ -54,35 +25,27 @@ angular.module('optimizer.spreadsheet.controller', [])
             console.log(err);
           });
       };
-      $scope.moveColorClass = function () {
-        var ret = '';
-        if ($rootScope.player.settings.spreadsheet.enableMoveColors) ret = 'enable-move-colors';
-        return ret;
+      
+      /**
+       * Pokemon Selection Modal
+       */
+      
+      $scope.selectedPokemon = null;
+      
+      $scope.openPokemonModal = function (pokemon) {
+        $scope.selectedPokemon = pokemon;
+        $('#pokemon-modal').modal('show');
       };
-      $scope.ivColorClass = function () {
-        var ret = '';
-        if ($rootScope.player.settings.spreadsheet.enableIVColors) ret = 'enable-iv-colors';
-        return ret;
-      };
-      var selectedArr = [];
-      $scope.toggleSelected = function (pokemon) {
-        var index = selectedArr.indexOf(pokemon);
-        if (index === -1) selectedArr.push(pokemon);
-        else selectedArr.splice(index, 1);
-      };
-      $scope.isSelected = function (pokemon) {
-        return selectedArr.indexOf(pokemon) !== -1;
-      };
-
+      
       /**
        * Sorting
        */
-
+      
       $('.ui.dropdown').dropdown();
-
+      
       $scope.sort     = [];
       $scope.sortArr  = [];
-
+      
       $scope.sortChange = function () {
         if ($scope.sortArr.length < $scope.sort.length) {
           for (var i = 0; i < $scope.sort.length; i++) {
@@ -96,19 +59,19 @@ angular.module('optimizer.spreadsheet.controller', [])
           }
         }
       };
-
+      
       $scope.getSortArray = function () {
         var ret = ['+metadata.id', '-metadata.piv'];
         if ($scope.sortArr.length > 0) ret = $scope.sortArr;
         return ret;
       };
-
+      
       /**
        * Searching
        */
-
+      
       $scope.search = '';
-
+      
       $scope.getSearchFilter = function () {
         var ret = '';
         if ($scope.search.length > 0 && $scope.search.indexOf(',') === -1) {
@@ -116,6 +79,55 @@ angular.module('optimizer.spreadsheet.controller', [])
         }
         return ret;
       };
-
+      
+      /**
+       * Styling
+       */
+      
+      $scope.formatPercentage = function (float) {
+        return Math.round(float * 100);
+      };
+      
+      $scope.formatMetric = function (float) {
+        return Math.round(float * 100) / 100;
+      };
+      
+      $scope.formatMoveName = function (name) {
+        var ret = '';
+        var spl = name.split('_');
+        for (var i = 0; i < spl.length; i++) {
+          if (i !== 0 && spl[i] !== 'FAST') ret += ' ';
+          if (spl[i] !== 'FAST') ret += spl[i].toLowerCase();
+        }
+        return ret;
+      };
+      
+      $scope.getMoveDPS = function (move) {
+        var ret = 0;
+        if (move.Power && move.DurationMs) {
+          var dps = parseInt(move.Power)/parseInt(move.DurationMs)*1000;
+          ret = dps.toFixed(2);
+        }
+        return ret;
+      };
+      
+      $scope.favoriteClass = function (pokemon) {
+        var ret = '';
+        if (pokemon.data.favorite) ret = 'favorite';
+        return ret;
+      };
+      
+      $scope.moveColorClass = function () {
+        var ret = '';
+        if ($rootScope.player.settings.spreadsheet.enableMoveColors) ret = 'enable-move-colors';
+        return ret;
+      };
+      
+      $scope.ivColorClass = function () {
+        var ret = '';
+        if ($rootScope.player.settings.spreadsheet.enableIVColors) ret = 'enable-iv-colors';
+        return ret;
+      };
+      
     }
   ]);
